6f093eed7966db5762d268391922a44852b65e7f
TatsuKenshi <rockinbg@gmail.com>
2022-11-01 20:48:57 +0100

    initial commit, sass basics

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/MainPage.scss";
+
+const MainPage = () => {
+  return (
+    <div className="mainPage">
+      <h1>Welcome to the Jungle</h1>
+      <button>click here</button>
+    </div>
+  );
+};
+
+export default MainPage;
